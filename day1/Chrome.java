593bcbe6642bdc7c978c88b62fdea62c5bffd13a
Sowmiyabai <sowmiyabai94@gmail.com>
2025-05-24 19:39:27 +0530

    Add files via upload
    Day1 Assignment

---
+++ day1/Chrome.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Chrome {
+	public static void main(String[] args) {
+		float version = 91.0F;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println("The float variable value is "+version);
+		System.out.println("The string variable value is "+developer);
+		System.out.println("The boolean variable value is "+isBeta);
+		System.out.println("The integer variable value is "+releaseYear);
+		System.out.println("The character variable value is "+shortcutKey);
+	}
+
+}
